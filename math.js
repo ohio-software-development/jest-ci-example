361d995f260a399c0aa443eed2c8fd7c9b211772
Brady Phelps <bp309420@ohio.edu>
2025-02-24 18:50:35 -0500

    baby's first commit

--- math.js
+++ math.js
@@ -11,8 +11,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) throw new Error("Cannot divide by zero");
+  if(b == 0){return 5;}
   return a / b;
+  
 }
 
 module.exports = { add, subtract, multiply, divide };
---
+++ math.test.js
@@ -0,0 +1,28 @@
+const {add,subtract, multiply, divide} = require('./math')
+
+describe('Math.js', () => {
+  describe('add', () => {
+    it("adds two numbers", () =>{
+      expect(add(1,2)).toBe(3);
+    });
+  });
+  describe('subtract', () => {
+    it('subtracts two numbers', ()=>{
+      expect(subtract(5,3)).toBe(2);
+    });
+  });
+  describe('multiply', () => {
+    it("multiplies two numbers", () =>{
+      expect(multiply(3,4)).toBe(12);
+    });
+  });  
+describe('divide', () => {
+    it("Divides two numbers", () =>{
+      expect(divide(10,2)).toBe(5);
+    });
+    it("throws an err when dividing by zero", () =>{
+      expect(()=>divide(5,0)).toThrow('Cannot divide by zero')
+    });
+  });
+  
+})  
